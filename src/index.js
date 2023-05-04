4c2940c00ce6ecadaaad41d3da0c329fb1e8dcb7
Mai <garcia.ton94@gmail.com>
2023-05-04 11:08:34 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submit = document.querySelector("#submit");
+
+  const toDo = document.querySelector("#new-task-description");
+  
+  const toDoList = document.querySelector("#tasks");
+
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const inputValue = toDo.value;
+    const listItem = document.createElement('li');
+    listItem.textContent = `${inputValue}`;
+    toDoList.append(listItem);
+    toDo.value = "";
+  }
+  submit.addEventListener("click", handleSubmit);
+
+
 });
+
